2684b97cf50d1d604965590279024c2f38d50fc6
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-22 21:38:38 +0300

    fix: app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,22 @@
 import { useState } from 'react';
+import { ThemeProvider } from '@gravity-ui/uikit';
 import { Header } from './components/Header';
-import { Tabs, type Tab } from './components/Tabs';
+import { Tabs, type TabValue } from './components/Tabs';
 import { UsbFlasher } from './components/UsbFlasher';
 import { WifiFlasher } from './components/WifiFlasher';
+import { useTheme } from './hooks/useTheme';
 import './App.css';
 
 function App() {
-  const [tab, setTab] = useState<Tab>('usb');
+  const [tab, setTab] = useState<TabValue>('usb');
+  const { theme, setTheme } = useTheme();
 
   return (
-    <>
-      <Header />
+    <ThemeProvider theme={theme}>
+      <Header onThemeChange={setTheme} />
       <Tabs tab={tab} onChange={setTab} />
       <main className="app-main">{tab === 'usb' ? <UsbFlasher /> : <WifiFlasher />}</main>
-    </>
+    </ThemeProvider>
   );
 }
 
